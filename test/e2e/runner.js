a57f573dde45fe4c4c101adba6d47e8619071abc
aloo <aloocs@gmail.com>
2017-05-31 16:03:01 +0800

    revert to semi

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -1,34 +1,34 @@
-var path = require('path');
-var spawn = require('cross-spawn'); // spawn child process
-var httpServer = require('http-server');  // simple server
+var path = require('path')
+var spawn = require('cross-spawn')
+var httpServer = require('http-server')
 var server = httpServer.createServer({
-  root: path.resolve(__dirname, '../../') // root directory
-});
+  root: path.resolve(__dirname, '../../')
+})
 
-server.listen(8080); // 8080 port
+server.listen(8080)
 
-var args = process.argv.slice(2); // split arguments
-if (args.indexOf('--config') === -1) { // lack of config
-  args = args.concat(['--config', 'build/nightwatch.config.js']); // default config
+var args = process.argv.slice(2)
+if (args.indexOf('--config') === -1) {
+  args = args.concat(['--config', 'build/nightwatch.config.js'])
 }
-if (args.indexOf('--env') === -1) { // prevent lack of config
-  args = args.concat(['--env', 'chrome,phantomjs']); // default browser launcher
+if (args.indexOf('--env') === -1) {
+  args = args.concat(['--env', 'chrome,phantomjs'])
 }
-var i = args.indexOf('--test');
+var i = args.indexOf('--test')
 if (i > -1) {
-  args[i + 1] = 'test/e2e/specs/' + args[i + 1] + '.js' // specfic test folder
+  args[i + 1] = 'test/e2e/specs/' + args[i + 1] + '.js'
 }
 
 var runner = spawn('./node_modules/.bin/nightwatch', args, {
   stdio: 'inherit'
-});
+})
 
-runner.on('exit', function (code) { // exit logic
-  server.close();
+runner.on('exit', function (code) {
+  server.close()
   process.exit(code)
-});
+})
 
-runner.on('error', function (err) { // error logic
-  server.close();
+runner.on('error', function (err) {
+  server.close()
   throw err
-});
+})
